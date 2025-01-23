4328bb312e11a1a546f5149f6db1ae1561bfa82a
AdenChen27 <AdenChen2005@gmail.com>
2025-01-22 19:01:49 -0600

    added notes

---
+++ winter/meeting2-notes.py
@@ -0,0 +1,32 @@
+# Access the notebooks at: https://github.com/AdenChen27/Oeconomica2024-AssetPricing/tree/main/winter
+
+# Review:
+# Tuples:
+my_tuple = (1, 2, 3)
+# Lists:
+my_list = [1, 2, 3]
+# Lists can be modified through e.g. my_list[2] = 4, tuples cannot.
+
+# Dictionaries:
+d = {'key1': 'value1', 'key2': 'value2'}
+
+
+# Loops:
+for i in range(5):
+    print(i)
+
+# e.g.:
+x = l[0]
+for i in l:
+    if i < x:
+        x = i
+
+function_name(argument1, argument2)
+
+
+def function_name(argument1, argument2):
+    return argument1 + argument2
+
+if function_name(2, 5) == 7:
+    print('It works!')
+
